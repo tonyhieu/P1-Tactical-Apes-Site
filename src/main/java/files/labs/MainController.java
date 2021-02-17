aa10f76e53070f083fbc6491f097087ca1d62362
tonyhieu <amv7668@gmail.com>
2021-02-16 17:14:13 -0800

    finished a lot of stuff lol

---
+++ src/main/java/files/labs/MainController.java
@@ -0,0 +1,24 @@
+package files.labs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+public class MainController {
+    @GetMapping("/team")
+    public String team() {
+        return "pages/team";
+    }
+}
