2d90dfe1861c649d55eea45ab58a1e6fc51cdbed
EyaadM <54916157+EyaadM@users.noreply.github.com>
2021-02-16 18:24:40 -0800

    Create Caesar.java

---
+++ src/main/java/files/labs/Caesar.java
@@ -0,0 +1,27 @@
+public class Caesar {
+  public void caesar(int shift, String phrase) {
+      for (int i = 0; i < phrase.length(); i++) {
+        if (containsSpecialCharacter(phrase.substring(i,i+1))) {
+          System.out.print(phrase.charAt(i));
+          continue;
+        }
+        char c = phrase.charAt(i);
+        if (c>='a' && c<=('z'-shift)) {
+          c+=shift;
+        }
+        else if (c>='A' && c<=('Z'-shift)) {
+          c+=shift;
+        }
+        else if (c>=('Z'-(shift-1)) && c<='Z') {
+          c-=shift;
+        }
+        else if (c>=('z'-(shift-1)) && c<='z') {
+          c-=shift;
+        }
+        System.out.print(c);
+      }
+  }
+  public boolean containsSpecialCharacter(String s) {
+      return (s == null) ? false : s.matches("[^A-Za-z0-9 ]");
+  }
+}
