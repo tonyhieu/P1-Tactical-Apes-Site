5c0cef94dbf88cb59952bfcab1aaf144c28d6cf2
tonyhieu <amv7668@gmail.com>
2021-03-07 22:44:25 -0800

    databases

---
+++ src/main/java/files/labs/modelsSQL/Player.java
@@ -0,0 +1,47 @@
+package files.labs.modelsSQL;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+/*
+Player is a POJO, Plain Old Java Object.
+First set of annotations add functionality to POJO
+--- @Setter @Getter @ToString @NoArgsConstructor @RequiredArgsConstructor
+The last annotation connect to database
+--- @Entity
+ */
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    /*
+    @NonNull: Places this in @RequiredArgsConstructor
+    @Size(min=2, max=30): Allows names between 2 and 30 characters long.
+     */
+    @NonNull
+    @Size(min = 2, max = 30, message = "Name (2 to 30 chars)")
+    private String name;
+
+    @NonNull
+    @Size(min = 2, max = 30, message = "Champion (2 to 30 chars)")
+    private String champion;
+
+    // Mastery is restricted
+    @NonNull
+    @Min(0) @Max(7)
+    private Integer mastery;
+}
